bc02025173665560d809159de6266477e1ea54bf
1Ankitk <110518334+1Ankitk@users.noreply.github.com>
2023-09-26 21:21:33 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,3 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import Heading from './Heading';
-// import Para from "./Para";
-// import List from './List';
-
-// function App() {
-//     return (<>
-//         <Heading/>
-//         <Para/> 
-//         <List/>
-//     </>);
-// }
-// export default App;
-
-
 import React from "react";
 function App(props){
     console.log(props);
